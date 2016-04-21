5d290b11f13694daba3a13731e04f687f444139b
hcnode <7372@163.com>
2016-04-21 12:35:39 +0800

    add front and back

--- js/front.js
+++ js/front.js
@@ -9,6 +9,20 @@ app.controller("main", ['$scope', '$timeout','$http', function ($scope, $timeout
 	$http.get("/getplaylist").then(response => {
 		$scope.data = response.data;
 	});
+	$scope.downloadOrPlay = item => {
+		if(item.isDownload){
+			window.open("/video/" + item.title + '.mp4');
+		}else {
+			item.isDownloading = true;
+			$http.post("/download_by_back", {
+				link: item.link,
+				title: item.title
+			}).then(response => {
+				item.isDownload = true;
+				item.isDownloading = false;
+			});
+		}
+	}
 }]).filter('to_trusted', ['$sce', function ($sce) {
 	return function (text) {
 		return $sce.trustAsHtml(text);
